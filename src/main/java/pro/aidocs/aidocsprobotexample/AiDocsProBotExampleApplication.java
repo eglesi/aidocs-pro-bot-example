4976fce5a0eb310f074159da5ba06daa0b348e23
Egle <hello@aidocs.pro>
2025-07-15 18:48:21 +0300

    initial commit

---
+++ src/main/java/pro/aidocs/aidocsprobotexample/AiDocsProBotExampleApplication.java
@@ -0,0 +1,13 @@
+package pro.aidocs.aidocsprobotexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDocsProBotExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiDocsProBotExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/aidocs/aidocsprobotexample/AiDocsProBotExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.aidocs.aidocsprobotexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDocsProBotExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
